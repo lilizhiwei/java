9a4421e54fe00f7c6da541bcdcf78b6aba20c2e2
lizhiwei <1548790965@qq.com>
2017-05-11 10:52:34 +0800

    上传代码

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/3f/50abeaf3f2350017119ad1ebc0c35329
@@ -0,0 +1,20 @@
+package java2;
+
+class Animal{
+    void go(){
+        System.out.println("animal go");
+    }
+}
+
+class Dog extends Animal{
+    void go(){
+        //调用父类方法
+        super.go();
+    }
+}
+
+//驱动函数
+public static void  main(String[] args){
+    Dog dog=new Dog();
+    dog.go();
+}
--- java2/src/java2/Animal.java
+++ java2/src/java2/Animal.java
@@ -1,20 +1,22 @@
 package java2;
 
-class Animal{
+class Dd{
     void go(){
         System.out.println("animal go");
     }
 }
 
-class Dog extends Animal{
+class Dog extends Dd{
     void go(){
         //调用父类方法
         super.go();
     }
 }
 
-//驱动函数
-public static void  main(String[] args){
-    Dog dog=new Dog();
-    dog.go();
+public class Animal {
+	//驱动函数
+	public static void  main(String[] args){
+	    Dog dog=new Dog();
+	    dog.go();
+	}
 }
